4564fa2cf8a5cd7b771fe2b780afd1cdf042e72a
Rockncoder <rockncoder@gmail.com>
2016-07-16 13:28:57 -0700

    Adds missing templates

--- src/hello.ts
+++ src/hello.ts
@@ -2,14 +2,8 @@ import {Component} from 'angular2/core';
 
 // annotations come from TS
 @Component({
-  selector: 'hola',
+  selector: 'jen',
   templateUrl: './templates/hello.html'
-//   template: `
-// <fieldset>
-//   <h2>Hello {{name}}, South Bay.</h2><br/>
-//   <input [(ngModel)]="name"  type="text"/>
-//   <button type="button" (click)="clickHandler()">Click Me!</button>
-// </fieldset>`
 })
 
 // classes come from es6
---
+++ src/home.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'home',
+  template: './templates/home.html'
+})
+
+export class HomeComponent {
+  constructor(){
+
+  }
+}
