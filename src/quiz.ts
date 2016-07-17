8e8f8a40e47a6ac756899562e59c70eef1ef21e7
Rockncoder <rockncoder@gmail.com>
2016-07-17 11:42:11 -0700

    adds the message component

--- src/quiz.ts
+++ src/quiz.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from 'angular2/core';
 import {QuizService} from './quiz-service';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {MessageComponent} from './message';
 
 @Component({
   selector: 'quiz',
   templateUrl: './templates/quiz.html',
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, MessageComponent],
   providers: [QuizService]
 })
 
