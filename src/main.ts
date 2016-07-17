e84421756625c7c08aaa80dcde114ae987bf665f
Rockncoder <rockncoder@gmail.com>
2016-07-17 10:09:57 -0700

    Adds mock backend

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
 import {bootstrap} from 'angular2/platform/browser'
-import {HomeComponent} from 'src/home'
+import {HomeComponent} from 'src/home';
+import {provide} from 'angular2/core';
+import {MockBackend} from 'angular2/http/testing';
+import {HTTP_PROVIDERS, Http, BaseRequestOptions} from 'angular2/http';
+import {FakeWebServer} from './fake-webserver';
 
-bootstrap(HomeComponent);
+
+bootstrap(HomeComponent, [
+  HTTP_PROVIDERS,
+  BaseRequestOptions,
+  MockBackend,
+  FakeWebServer,
+  provide(Http, {
+    deps: [MockBackend, BaseRequestOptions],
+    useFactory: (backend, options) => {
+      return new Http(backend, options);
+    }
+  })
+]);
